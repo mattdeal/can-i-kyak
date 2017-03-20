fc49426d0b20974003ce1c94ceca796aa450e8f2
mwdeal1 <mwdeal1@gmail.com>
2017-03-20 19:19:29 -0400

    Add placeholder routes

--- routes/api-routes.js
+++ routes/api-routes.js
@@ -5,11 +5,42 @@ var models = require("../models");
 
 module.exports = function(app) {
 
+  // for testing purposes
   app.post("/info", function(req,res) {
     var test = "daniel";
     res.json(test);
   });//end app.post
 
+  //todo: fill in these functions
 
+  //todo: get all difficulties by googleId
+  app.get("/difficulty/:googleId", function(req, res) {
+
+  });
+
+  //todo: get all comments by googleId
+  app.get("/comment/:googleId", function(req, res) {
+
+  });
+
+  //todo: get all rentals by googleId
+  app.get("/rental/:googleId", function(req, res) {
+
+  });
+
+  //todo: create difficulty
+  app.post("/difficulty", function(req, res) {
+
+  });
+
+  //todo: create comment
+  app.post("/comment", function(req, res) {
+
+  });
+
+  //todo: create rental
+  app.post("/rental", function(req, res) {
+
+  });
 
 };//end module.exports
