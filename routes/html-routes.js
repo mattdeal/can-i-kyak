a77c56c270f44b9fbd379fd3209f96bb009fddc5
mwdeal1 <mwdeal1@gmail.com>
2017-03-27 15:19:13 -0400

    Add html routes and finish handlebars files

--- routes/html-routes.js
+++ routes/html-routes.js
@@ -1,5 +1,6 @@
 //Dependencies
 var path = require("path");
+var models = require("../models");
 
 //Routes
 module.exports = function(app) {
@@ -7,6 +8,25 @@ module.exports = function(app) {
   // default to home
   app.get("/", function(req, res) {
     res.sendFile(path.join(__dirname, "/../public/view.html"));
+    //todo: could res.render() work here?  - insert map contents into main template
+  });
+
+  // route for creating a new location
+  app.get('/add', function(req, res) {
+    //todo: could res.render() work here?  - insert add contents into main template
+    res.sendFile(path.join(__dirname, "/../public/addition.html"));
+  });
+  
+  // route to lookup locations by state
+  app.get('/locations/:state', function(req, res) {
+    //todo: db queries
+    res.render('results', {});
+  });
+
+  // route to lookup a single location 
+  app.get('/location/:id', function(req, res) {
+    //todo: db queries
+    res.render('location', {});
   });
 
 };//end module.exports
