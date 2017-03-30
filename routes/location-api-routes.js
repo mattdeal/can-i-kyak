f7de1aac829f048dfbd734d61ce78cb761d545cd
Daniel Oliveira <dnlsoliveira@gmail.com>
2017-03-29 21:34:58 -0400

    added add routes

--- routes/location-api-routes.js
+++ routes/location-api-routes.js
@@ -4,20 +4,23 @@ var models = require("../models");
 // Routes
 module.exports = function(app) {
 
-  app.get("/api/locations/:state", function(req, res) {
-    if (req.params.state) {
-      models.Location.findAll({
-        where: {
-          state: req.params.state
-        }
-      }).then(function(modelsLocation) {
-        // console.log(modelsLocation);
-        // res.json(modelsLocation);
+  //creates new place in database
+  app.post("/api/location", function(req, res) {
 
-        // console.log(modelsLocation);
-        //
-        res.render("results", {locations: modelsLocation});
-      });
-    }
-  }); //end app.get
+    models.Location.create({
+      name: req.body.name,
+      street: req.body.street,
+      city: req.body.city,
+      state: req.body.state,
+      zip: req.body.zip,
+      googleId: req.body.googleId,
+      imageUrl: req.body.imageUrl
+    })
+    .then(function(newPlace) {
+      res.json(newPlace);
+      
+    });
+
+  });//end app.post
+
 }; //end module.exports
